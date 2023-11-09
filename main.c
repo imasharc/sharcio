3156474c9715667812f1b322220d326840a67e54
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-11-09 21:55:33 +0100

    feat(main): print current formatted date
    get the current time in seconds since the Epoch: 1st January 1900
    convert time_t value t into broken-down structure
    create buffer for the formatted data
    format the string
    print to make sure

---
+++ main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+/*
+ * Bare with the comments, bc they explain (to me) what's going on
+ */
+
+int main()
+{
+ time_t t = time(NULL); // returns current time in sec since the Epoch
+ struct tm tm = *localtime(&t); // converts time_t value into broken-down time structure
+ char formattedString[20]; // create buffer for the formatted data
+ sprintf(formattedString, "%02d-%02d-%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900); // format the string
+ printf("Formatted Date: %s", formattedString); // print to make sure
+}
