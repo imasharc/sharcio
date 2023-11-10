6d71dc3982633a18ada18835a8401587a898c7e2
Antoni Malinowski <antonimalinowski.kontakt@gmail.com>
2023-11-10 16:44:40 +0100

    fix(main): fix formatting of the written file title
    remove the newline characterfrom formattedString
    
    change the cli argument parsing to ./main.exe -t 'this is the title' so that the written file is '11-10-2023_this is the title.txt'

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
  time_t t = time(NULL); // returns current time in sec since the Epoch
  struct tm tm = *localtime(&t); // converts time_t value into broken-down time structure
  char formattedString[20]; // create buffer for the formatted data
- sprintf(formattedString, "%02d-%02d-%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900); // format the string
+ sprintf(formattedString, "%02d-%02d-%d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900); // format the string
  
  char filename[256]; // Adjust the buffer size as needed
  snprintf(filename, sizeof(filename), "%s_%s.txt", formattedString, title);
